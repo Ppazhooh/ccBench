5f4e4dbed7db4329c4d7c9a46d484d6bb2f4d5ed
Parsa Pazhooheshy <parsapzh@node-0.test.cc4ngn.emulab.net>
2025-04-10 14:41:25 -0600

    the measurment loop in c is not accurate, thus we need to modify the oracle based on that, not fixed steps

--- pantheon-modified/src/analysis/network_oracle.py
+++ pantheon-modified/src/analysis/network_oracle.py
@@ -5,74 +5,80 @@ import sys
 def analyze_trace_file(filename):
     print('READING: ' + filename)
     
+    # Read the trace file once
     with open(filename) as file:
-        lines = file.readlines()
-        lines = lines[7:-1]
+        lines = file.readlines()[7:-1]  # Skip first 7 and last line
         lines = [line.rstrip() for line in lines]
 
     print('FINISHED READING')
-    df = pd.DataFrame(columns=['time', 'channel_capacity', 'avg_queuing_delay', 'packets_change'])
-    print(filename)
-    # cwnd_file = filename.split('/')[-2].replace("single-flow-scenario-", "").replace("-", "_")
+
+    # Extract the cwnd file path
     cwnd_file = filename.split('/')[-2].replace("single-flow-scenario-", "")
-    cwnd_file_full_path = '/d1/ccBench/pantheon-modified/third_party/tcpdatagen/dataset/' + cwnd_file + '-cwnd.txt'
-    
+    cwnd_file_full_path = '/d1/ccBench/pantheon-modified/third_party/tcpdatagen/dataset/{}-cwnd.txt'.format(cwnd_file)
 
-    times = []
-    channel_capacities = []
-    avg_queuing_delays = []
-    packets_change = []
-
-    time_step = 10 # in ms
+    # Read the cwnd file once
     with open(cwnd_file_full_path, "r") as f:
-        first_line = f.readline()
-        t0 = (float(first_line.split()[0])*1000) -  time_step
+        times = [float(line.split()[0]) * 1000 for line in f.readlines()]  # Convert to ms in one pass
 
-   
-    last_time = float(lines[-1].split()[0])
+    # Pre-allocate lists for DataFrame
+    time_step = 10  # in ms
+    num_points = len(times)
+    
+    times_df = np.zeros(num_points)
+    channel_capacities = np.zeros(num_points)
+    avg_queuing_delays = np.zeros(num_points)
+    packets_change = np.zeros(num_points)
 
-    start_time = t0 
-    end_time = t0 + time_step
-
-    while end_time < last_time:
+    # Process the data
+    for i in range(num_points):
+        start_time = times[i] - time_step
+        end_time = times[i]
+        
+        # Select lines within the time window
         selected_lines = [line for line in lines if start_time <= float(line.split()[0]) < end_time]
-
+        
+        # Calculate channel capacity
         channel_bytes = sum(int(line.split()[2]) for line in selected_lines if '#' in line.split()[1])
+        channel_capacity = (channel_bytes / time_step) * 8 * 1000 / 1000000  # Convert to Mbps
 
-        channel_capacity = (channel_bytes / time_step) * 8* 1000 / 1000000  # Convert to Mbps
-
+        # Calculate avg queuing delay
         egress_lines = [line for line in selected_lines if '-' in line.split()[1]]
-        egress_packts = len(egress_lines)
+        egress_packets = len(egress_lines)
         delay = sum(int(line.split()[3]) for line in egress_lines)
-        avg_queuing_delay = delay / egress_packts if egress_packts else 0
+        avg_queuing_delay = delay / egress_packets if egress_packets else 0
 
-        ingress_packts = sum(1 for line in selected_lines if '+' in line.split()[1])
-        change = egress_packts - ingress_packts
+        # Calculate packet change
+        ingress_packets = sum(1 for line in selected_lines if '+' in line.split()[1])
+        change = egress_packets - ingress_packets
 
-        times.append(end_time)
-        channel_capacities.append(channel_capacity)
-        avg_queuing_delays.append(avg_queuing_delay)
-        packets_change.append(change)
+        # Store the results
+        times_df[i] = end_time
+        channel_capacities[i] = channel_capacity
+        avg_queuing_delays[i] = avg_queuing_delay
+        packets_change[i] = change
 
-        start_time = end_time
-        end_time += time_step
+    # Create DataFrame from the pre-allocated arrays
+    df = pd.DataFrame({
+        'time': times_df,
+        'channel_capacity': channel_capacities,
+        'avg_queuing_delay': avg_queuing_delays,
+        'packets_change': packets_change
+    })
 
-    df['time'] = times
-    df['channel_capacity'] = channel_capacities
-    df['avg_queuing_delay'] = avg_queuing_delays
-    df['packets_change'] = packets_change
     print('FINISHED ANALYSIS')
 
     output_filename = filename.split('/')[-2] + '.csv'
     output_filename = output_filename.replace('single-flow-scenario-', '')
-        
     output_base_address = '/d1/ccBench/pantheon-modified/third_party/tcpdatagen/dataset/'
     print(output_filename)
-    df.to_csv( output_base_address + output_filename, index=False)
+
+    # Write the DataFrame to CSV in one call
+    df.to_csv(output_base_address + output_filename, index=False, header=False)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: python script.py <trace_file>")
         sys.exit(1)
+    
     trace_filename = sys.argv[1]
-    analyze_trace_file( trace_filename + 'tcpdatagen_mm_datalink_run1.log')
+    analyze_trace_file(trace_filename + 'tcpdatagen_mm_datalink_run1.log')
