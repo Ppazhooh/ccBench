0da5f4d56a495ec2c4010bcb8a58f91523e5eb32
Parsa Pazhooheshy <75143001+Ppazhooh@users.noreply.github.com>
2025-04-23 23:23:51 -0400

    Solving python2 compatibility issue

--- pantheon-modified/src/analysis/network_oracle.py
+++ pantheon-modified/src/analysis/network_oracle.py
@@ -1,17 +1,18 @@
+from __future__ import print_function, division
 import numpy as np
 import pandas as pd
 import sys
 import bisect
 
 def analyze_trace_file(filename):
-    print('READING: ' + filename)
+    print('READING:', filename)
 
-    # Read trace, skipping header and footer
-    with open(filename) as file:
-        raw_lines = file.readlines()[7:-1]
-    print('FINISHED READING')
+    # Read trace, skipping header (first 7) and footer (last line)
+    with open(filename, 'r') as f:
+        raw_lines = f.readlines()[7:-1]
+    print('FINISHED READING TRACE')
 
-    # Parse trace lines once
+    # Parse trace lines
     parsed_lines = []
     timestamps = []
     for line in raw_lines:
@@ -23,155 +24,162 @@ def analyze_trace_file(filename):
         parsed_lines.append((t, label, size, delay))
         timestamps.append(t)
 
-    # Extract scenario parameters from path
-    # e.g. .../single-flow-scenario-100-200/...
-    dirname = filename.split('/')[-2]
-    mRTT = int(dirname.split('-')[-3])
-    capacity_pkts = int(dirname.split('-')[-2])  # initial buffer capacity in packets
+    # Extract scenario parameters from parent directory
+    dirname = filename.split('/')[-2]  # e.g. "single-flow-scenario-100-200"
+    capacity_pkts = int(dirname.split('-')[-2])  # buffer capacity in packets
+    mRTT = int(dirname.split('-')[-3])           # in ms
 
     # Read cwnd timestamps (ms)
     cwnd_id = dirname.replace("single-flow-scenario-", "")
-    cwnd_file = f'/d1/ccBench/pantheon-modified/third_party/tcpdatagen/dataset/{cwnd_id}-cwnd.txt'
-    with open(cwnd_file) as f:
-        times = [float(line.split()[0]) * 1000 for line in f]
+    cwnd_file = '/d1/ccBench/pantheon-modified/third_party/tcpdatagen/dataset/{0}-cwnd.txt'.format(cwnd_id)
+    with open(cwnd_file, 'r') as f:
+        times = np.array([float(line.split()[0]) * 1000 for line in f])  # ms
 
-    time_step = 10.0  # ms window size
+    time_step = 10.0  # ms
     num_points = len(times)
+    T_end = times[-1]
 
     # Pre-allocate arrays for metrics
-    times_df                  = np.zeros(num_points)
-    channel_capacities        = np.zeros(num_points)
-    avg_queuing_delays        = np.zeros(num_points)
-    free_buffer_pkts          = np.zeros(num_points)
-    drop_rates                = np.zeros(num_points)
-    rec_rates                 = np.zeros(num_points)
-    mRTTs                     = np.full(num_points, mRTT)
+    times_df                   = np.zeros(num_points)
+    channel_capacities         = np.zeros(num_points)
+    avg_queuing_delays         = np.zeros(num_points)
+    queueing_delay_gradients   = np.zeros(num_points)
+    free_buffer_pkts           = np.zeros(num_points)
+    buffer_occupancies_bytes   = np.zeros(num_points)
+    queue_length_var_bytes     = np.zeros(num_points)
+    drop_rates                 = np.zeros(num_points)
+    loss_burst_length_pkts     = np.zeros(num_points)
+    rec_rates                  = np.zeros(num_points)
+    inter_arrival_jitter_ms    = np.zeros(num_points)
+    mRTTs                      = np.full(num_points, mRTT)
+    time_to_buffer_full_ms     = np.zeros(num_points)
+    link_busyness_fraction     = np.zeros(num_points)
 
-    # New metrics
-    loss_burst_lengths        = np.zeros(num_points)
-    inter_arrival_jitters     = np.zeros(num_points)
-    buffer_occupancies        = np.zeros(num_points)
-    queue_length_variances    = np.zeros(num_points)
-    queueing_delay_gradients  = np.zeros(num_points)
-
-    # Track current occupancy in packets and bytes
+    # State for occupancy tracking
     occupancy_pkts = 0
-    buffer_bytes   = 0
 
-    # Slide through each window
     for i in range(num_points):
-        start_time = times[i] - time_step
-        end_time   = times[i]
+        start_t = times[i] - time_step
+        end_t   = times[i]
 
-        left_idx  = bisect.bisect_left(timestamps, start_time)
-        right_idx = bisect.bisect_left(timestamps, end_time)
-        window_events = parsed_lines[left_idx:right_idx]
+        # select events in [start_t, end_t)
+        left  = bisect.bisect_left(timestamps, start_t)
+        right = bisect.bisect_left(timestamps, end_t)
+        window_events = parsed_lines[left:right]
 
-        # Trackers
+        # Trackers for new metrics
         burst_lengths = []
         current_burst = 0
         arrival_times = []
         occupancy_samples = []
+        busy_ms_set = set()
 
-        # Accumulators
-        channel_bytes   = 0
-        egress_lines    = 0
-        delay_sum       = 0
-        drops           = 0
-        ingress_pkts    = 0
-        received_bytes  = 0
+        # Original accumulators
+        channel_bytes = 0
+        egress_pkts   = 0
+        delay_sum     = 0
+        drops         = 0
+        ingress_pkts  = 0
+        rec_bytes     = 0
 
         for (t, label, size, delay) in window_events:
-            # Channel throughput (#)
+            # channel throughput marker
             if '#' in label:
                 channel_bytes += size
+                busy_ms_set.add(int(t))  # mark this ms as busy
 
-            # Egress = successful delivery (-)
+            # successful egress ('-')
             if '-' in label:
-                egress_lines += 1
+                egress_pkts += 1
                 delay_sum += delay
-                received_bytes += size
+                rec_bytes += size
                 arrival_times.append(t)
-                # release bytes from queue
-                buffer_bytes = max(buffer_bytes - size, 0)
-
-                # end loss burst if active
+                # end any loss burst
                 if current_burst > 0:
                     burst_lengths.append(current_burst)
                     current_burst = 0
 
-            # Packet drop (d)
+            # drop ('d')
             elif 'd' in label:
                 drops += 1
                 current_burst += 1
 
-            # Ingress = enqueue (+)
+            # ingress ('+')
             elif '+' in label:
                 ingress_pkts += 1
-                buffer_bytes += size
-                occupancy_pkts += 1
 
-            # record occupancy sample
-            occupancy_samples.append(buffer_bytes)
+            occupancy_samples.append(occupancy_pkts)
 
-        # finalize burst
+        # finalize any ongoing burst
         if current_burst > 0:
             burst_lengths.append(current_burst)
 
-        # Compute original metrics
-        channel_capacity    = (channel_bytes / time_step) * 8 * 1000 / 1e6  # Mbps
-        avg_queuing_delay   = delay_sum / egress_lines if egress_lines else 0
-        drop_rate           = drops / (drops + egress_lines) if (drops + egress_lines) else 0
-        rec_rate            = (received_bytes / time_step) * 8 * 1000 / 1e6  # Mbps
+        # Update occupancy (packets)
+        net_pkts = ingress_pkts - egress_pkts
+        occupancy_pkts = min(max(occupancy_pkts + net_pkts, 0), capacity_pkts)
+        free_pkts = capacity_pkts - occupancy_pkts
 
-        # Update and clamp packet occupancy
-        occupancy_pkts += ingress_pkts - egress_lines
-        occupancy_pkts = min(max(occupancy_pkts, 0), capacity_pkts)
-        free_slots = capacity_pkts - occupancy_pkts
+        # Original metric computations
+        channel_capacity_mbps = (channel_bytes / time_step) * 8 * 1000 / 1e6
+        avg_q_delay_ms        = delay_sum / egress_pkts if egress_pkts else 0
+        drop_rate_val         = drops / (drops + egress_pkts) if (drops + egress_pkts) else 0
+        rec_rate_mbps         = (rec_bytes / time_step) * 8 * 1000 / 1e6
 
-        # Store metrics
-        times_df[i]                   = end_time
-        channel_capacities[i]         = channel_capacity
-        avg_queuing_delays[i]         = avg_queuing_delay
-        free_buffer_pkts[i]           = free_slots
-        drop_rates[i]                 = drop_rate
-        rec_rates[i]                  = rec_rate
+        # New metric: time to buffer full (ms), capped at end of trace
+        if net_pkts > 0:
+            remaining_pkts = capacity_pkts - occupancy_pkts
+            ttf = remaining_pkts / (net_pkts / time_step)
+            ttf = min(ttf, T_end - times[i])
+        else:
+            ttf = T_end - times[i]
 
-        # New metrics
-        loss_burst_lengths[i]        = np.mean(burst_lengths) if burst_lengths else 0
-        if len(arrival_times) > 1:
-            inter_arrival_jitters[i] = np.std(np.diff(arrival_times))
-        buffer_occupancies[i]       = buffer_bytes
-        queue_length_variances[i]   = np.var(occupancy_samples) if occupancy_samples else 0
-        if i > 0:
-            queueing_delay_gradients[i] = avg_queuing_delay - avg_queuing_delays[i-1]
+        # New metric: link busyness fraction in this window
+        busy_ms = len(busy_ms_set)
+        busyness = busy_ms / time_step
 
-    # Build DataFrame
-    df = pd.DataFrame({
-        'time':                    times_df,
-        'channel_capacity_Mbps':   channel_capacities,
-        'avg_queuing_delay_ms':    avg_queuing_delays,
+        # Store into arrays
+        times_df[i]                   = end_t
+        channel_capacities[i]         = channel_capacity_mbps
+        avg_queuing_delays[i]         = avg_q_delay_ms
+        queueing_delay_gradients[i]   = (avg_q_delay_ms - avg_queuing_delays[i-1]) if i > 0 else 0
+        free_buffer_pkts[i]           = free_pkts
+        buffer_occupancies_bytes[i]   = occupancy_pkts
+        queue_length_var_bytes[i]     = np.var(occupancy_samples) if occupancy_samples else 0
+        drop_rates[i]                 = drop_rate_val
+        loss_burst_length_pkts[i]     = np.mean(burst_lengths) if burst_lengths else 0
+        rec_rates[i]                  = rec_rate_mbps
+        inter_arrival_jitter_ms[i]    = np.std(np.diff(arrival_times)) if len(arrival_times) > 1 else 0
+        mRTTs[i]                      = mRTT
+        time_to_buffer_full_ms[i]     = ttf
+        link_busyness_fraction[i]     = busyness
+
+    # Assemble DataFrame
+    df_out = pd.DataFrame({
+        'time_ms':                    times_df,
+        'channel_capacity_Mbps':      channel_capacities,
+        'avg_queuing_delay_ms':       avg_queuing_delays,
         'queueing_delay_gradient_ms': queueing_delay_gradients,
-        'free_buffer_pkts':        free_buffer_pkts,
-        'buffer_occupancy_bytes':  buffer_occupancies,
-        'queue_length_var_bytes':  queue_length_variances,
-        'drop_rate':               drop_rates,
-        'loss_burst_length_pkts':  loss_burst_lengths,
-        'rec_rate_Mbps':           rec_rates,
-        'inter_arrival_jitter_ms': inter_arrival_jitters,
-        'mRTT_ms':                 mRTTs
+        'free_buffer_pkts':           free_buffer_pkts,
+        'buffer_occupancy_pkts':      buffer_occupancies_bytes,
+        'queue_length_var_pkts':      queue_length_var_bytes,
+        'drop_rate':                  drop_rates,
+        'loss_burst_length_pkts':     loss_burst_length_pkts,
+        'rec_rate_Mbps':              rec_rates,
+        'inter_arrival_jitter_ms':    inter_arrival_jitter_ms,
+        # 'time_to_buffer_full_ms':  time_to_buffer_full_ms,
+        'link_busyness_fraction':     link_busyness_fraction,
+        'mRTT_ms':                    mRTTs
     })
 
     # Write CSV
     output_name = dirname.replace('single-flow-scenario-', '') + '.csv'
-    out_path    = '/d1/ccBench/pantheon-modified/third_party/tcpdatagen/dataset/' + output_name
+    out_path = '/d1/ccBench/pantheon-modified/third_party/tcpdatagen/dataset/{0}'.format(output_name)
     print('WRITING:', out_path)
-    df.to_csv(out_path, index=False)
+    df_out.to_csv(out_path, index=False)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: python script.py <trace_file_base_without_.log>")
         sys.exit(1)
-
-    trace_base = sys.argv[1]
-    analyze_trace_file(trace_base + 'tcpdatagen_mm_datalink_run1.log')
+    base = sys.argv[1]
+    analyze_trace_file(base + 'tcpdatagen_mm_datalink_run1.log')
